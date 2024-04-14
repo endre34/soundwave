ee6196cc346e9354d2e6617bb31a48ef84bf92ce
Endre <farkas.endre511@gmail.com>
2024-04-14 07:32:55 -0400

    First version cpp

---
+++ src/SzalontaGrafika2024.cpp
@@ -0,0 +1,36 @@
+//============================================================================
+// Name        : SzalontaGrafika2024.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
